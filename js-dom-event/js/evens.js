9a82d09a544c4f9a959da042dd185cff72a9b6f2
sadhin28 <taosifsadhin113@gmail.com>
2025-04-05 13:30:47 +0600

    add blue and gray button

--- js-dom-event/js/evens.js
+++ js-dom-event/js/evens.js
@@ -1,1 +1,10 @@
 console.log('Js dom separet File')
+function makeRed(){
+            document.body.style.backgroundColor='red'
+        }
+
+const grayButton=document.getElementById('make-gray') ;
+grayButton.onclick=makegray;
+function makegray(){
+    document.body.style.backgroundColor='gray';
+}       
