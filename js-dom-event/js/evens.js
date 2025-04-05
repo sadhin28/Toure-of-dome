b3979aa779403514ea79dda4fa6e773df685beab
sadhin28 <taosifsadhin113@gmail.com>
2025-04-05 13:36:01 +0600

    add purple button

--- js-dom-event/js/evens.js
+++ js-dom-event/js/evens.js
@@ -1,10 +1,15 @@
 console.log('Js dom separet File')
 function makeRed(){
             document.body.style.backgroundColor='red'
-        }
+}
 
 const grayButton=document.getElementById('make-gray') ;
 grayButton.onclick=makegray;
 function makegray(){
     document.body.style.backgroundColor='gray';
 }       
+
+const purpleButton=document.getElementById('make-purple')
+purpleButton.onclick=function makePurple(){
+    document.body.style.backgroundColor='purple'
+}
