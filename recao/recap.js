7756dc985c03cac021124aa23f38907ae12afac4
sadhin28 <taosifsadhin113@gmail.com>
2025-04-06 22:41:50 +0600

    add coments

--- recao/recap.js
+++ recao/recap.js
@@ -1,7 +1,13 @@
 //step 1: addEventListener
 document.getElementById('btnClick').addEventListener('click', function(){
-    //step 2:
+    //step 2:get the coment inside the text area
     const coments = document.getElementById('new-coment');
-    console.log(coments.value)
-    
+    const newcoment = coments.value
+    //set the comment inside the commentbox
+    const comentbox = document.getElementById('comentBox')
+    const p =document.createElement('p');
+    p.innerText=newcoment.value;
+    comentbox.appendChild(p);
+    //step 4: clean coment box
+    coments.value=''
 })
