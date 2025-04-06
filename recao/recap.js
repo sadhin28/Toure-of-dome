adceb2edad942108288ed6f9a901d62d9229bd21
sadhin28 <taosifsadhin113@gmail.com>
2025-04-06 22:23:57 +0600

    add coment

--- recao/recap.js
+++ recao/recap.js
@@ -1,1 +1,7 @@
-document.getElementById('inputText').addEventListener
+//step 1: addEventListener
+document.getElementById('btnClick').addEventListener('click', function(){
+    //step 2:
+    const coments = document.getElementById('new-coment');
+    console.log(coments.value)
+    
+})
