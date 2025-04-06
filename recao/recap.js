13d0ae79f00bc1660f7bc402200a7220a62a290e
sadhin28 <taosifsadhin113@gmail.com>
2025-04-06 22:52:05 +0600

    add alert comment

--- recao/recap.js
+++ recao/recap.js
@@ -1,10 +1,12 @@
 //step 1: addEventListener
 document.getElementById('btnClick').addEventListener('click', function(){
+    alert('Are you sure ,you want to send this coment? Enter "OK"')
     //step 2:get the coment inside the text area
     const coments = document.getElementById('new-coment');
     const newcoment = coments.value
     //set the comment inside the commentbox
     const comentbox = document.getElementById('comentBox')
+    //stp 3: createElement and set innerText
     const p =document.createElement('p');
     p.innerText=newcoment;
     comentbox.appendChild(p);
